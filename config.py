7500911cc33a6a02b1f6f7bbd350f7d546d30e86
PlasmaSheep <plasmasheep@gmail.com>
2014-03-02 19:00:33 -0800

    added info to config file

--- config.py
+++ config.py
@@ -3,13 +3,15 @@ Configuration file for the pipeline.
 """
 from corenlp import StanfordCoreNLP
 
-
+# NLP locations
 CORE_NLP_DIR = "stanford-corenlp/"
 PARSER = StanfordCoreNLP(CORE_NLP_DIR)
 
+# Processing options
 grammatical_processing = True; 
 part_of_speech_tagging = True;
 word_to_word_similarity = True;
 sequence_indexing = True;
 
+# Database options
 db_url = 'sqlite:///wordseer.db'
