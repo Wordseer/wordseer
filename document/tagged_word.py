64b3471b257190c4c814416aee29cda540020b7f
PlasmaSheep <plasmasheep@gmail.com>
2014-03-05 20:16:25 -0800

    Added tagged_word class

---
+++ document/tagged_word.py
@@ -0,0 +1,22 @@
+class TaggedWord:
+    """
+    This class describes a single tagged word.
+    """
+    
+    space_before = " "
+    space_after = " "
+
+    def __init__(self, *args, **kwargs):
+        """Instantiate a TaggedWord object.
+
+        Kwargs:
+            id (int): The ID for the word.
+            word (str): The stored word.
+            tag (str): Part of speech for the word.
+            lemma (str): The lemma for this word.
+        """
+        for item, val in kwargs.items():
+            setattr(self, item, val)
+
+    def __str__(self):
+        return self.word + "/" + self.tag + " - " + lemma
