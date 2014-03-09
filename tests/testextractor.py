9f5a87259f1a4e6e556f487f556b92f0a42cddc2
PlasmaSheep <plasmasheep@gmail.com>
2014-03-08 17:41:20 -0800

    Started extractor tests

---
+++ tests/testextractor.py
@@ -0,0 +1,13 @@
+import unittest
+import structurextractor
+import tokenizer
+
+@unittest.skip("Not yet working")
+class ExtractorTests(unittest.TestCase):
+    def setUp(self):
+        self.structure_file = "data/occupy-collection-structure.json"
+        self.input_file =
+            open("casestudies/occupy/xml/Houston__1__CBS Houston.xml")
+        t = tokenizer.Tokenizer()
+        extractor = structurextractor.StructureExtractor(t, structure_file)
+        documents = extractor.extract(self.input_file)
