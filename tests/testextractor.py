1a72b87c64206b884f40ad2549785bce9f4b2b6c
PlasmaSheep <plasmasheep@gmail.com>
2014-03-09 21:15:42 -0700

    Fixed typo in extractor test

--- tests/testextractor.py
+++ tests/testextractor.py
@@ -2,22 +2,21 @@ import unittest
 import structureextractor
 import tokenizer
 
-@unittest.skip("Not yet working")
+#@unittest.skip("Not yet working")
 class ExtractorTests(unittest.TestCase):
     def setUp(self):
-        self.structure_file = "data/occupy-collection-structure.json"
-        self.input_file = \
-            open("casestudies/occupy/xml/Houston__1__CBS Houston.xml")
+        self.structure_file = "data/structure.json"
+        self.input_file = open("data/articles/post1.xml")
         t = tokenizer.Tokenizer()
         extractor = structurextractor.StructureExtractor(t, structure_file)
         documents = extractor.extract(self.input_file)
 
     def test_css_maker(self):
-        xpaths = {"./author/text()" = ":root > author > "
-            "./title/text()" = ":root > title > "
-            "./time/text()" = ":root > time > "
-            "./number/text()" = ":root > number > "
-            "./tags/tag/text()" = ":root > tags > tag > "}
+        xpaths = {"./author/text()": ":root > author > ",
+            "./title/text()": ":root > title > ",
+            "./time/text()": ":root > time > ",
+            "./number/text()": ":root > number > ",
+            "./tags/tag/text()": ":root > tags > tag > "}
         for path, selector in xpaths.items():
             self.failunless(selector == extractor.make_css_selector(path))
 
