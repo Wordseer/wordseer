d3df34dbeb8c844b6cbbed1d87d2a0b0e13cba9c
PlasmaSheep <plasmasheep@gmail.com>
2014-03-14 20:51:46 -0700

    Started on sequenceprocessor

---
+++ sequenceprocessor.py
@@ -0,0 +1,43 @@
+class SequenceProcessor(object):
+    def __init__(reader_writer, grammatical_info_exists):
+        # TODO: handle reader_writer once it's finished
+        self.grammatical_info_exists = grammatical_info_exists
+
+        stop_words.extend(pronouns + prepositions + determiners + \
+            conjunctions + modal_verbs + primary_verbs + adverbs + \
+            punctuation + contractions)
+
+    def join_lemmas(words, delimiter):
+        """Join the lemmas of words with the delimiter.
+
+        :param list words: A list of TaggedWord objects with lemmas.
+        :param str delimiter: A delimiter to put between lemmas.
+        :return str: The combined lemmas.
+        """
+
+        lemmas = ""
+        for word in words:
+            lemmas += word.lemma + delimiter
+
+        return lemmas
+
+    def join_words(words, delimiter):
+        """Join  words with the delimiter.
+
+        :param list words: A list of TaggedWord objects.
+        :param str delimiter: A delimiter to put between words.
+        :return str: The combined words.
+        """
+
+        words = ""
+        for word in words:
+            words += word.word + delimiter
+
+        return words
+
+    def remove_stops(words):
+        """Remove every sort of stop from the sentences.
+
+        :param list words: A list of TaggedWord objects.
+        :return:
+        without_stops = []
