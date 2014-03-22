88138ee32fb06eee1dc5264ee8704872890f4c6b
PlasmaSheep <plasmasheep@gmail.com>
2014-03-22 15:55:48 -0700

    Sped up unit tests

--- tests/testtokenizer.py
+++ tests/testtokenizer.py
@@ -1,31 +1,45 @@
+"""
+Tests for the tokenizer.
+"""
+
 import unittest
 import tokenizer
 from document import sentence
 
-class CommonTests:
-    @classmethod
-    def setUpClass(commonTests):
-            CommonTests.t = tokenizer.Tokenizer()
+t = tokenizer.Tokenizer()
 
+class CommonTests(object):
+    """Tests and variables common to all test cases.
+    """
     def setUp(self, text=""):
-            self.example = text
-            self.result = self.t.tokenize(self.example)
-            self.raw = self.t.parser.raw_parse(self.example)
+        """Set up some local variables.
+        """
+        self.example = text
+        self.result = t.tokenize(self.example)
+        self.raw = t.parser.raw_parse(self.example)
 
     def test_text(self):
+        """Test to make sure the text is accurately transscribed.
+        """
         for s in range(0, len(self.result)):
             for w in range(0, len(self.result[s].words)):
                 self.failUnless(self.result[s].words[w] ==
                     self.raw["sentences"][s]["words"][w][0])
 
     def test_tags(self):
+        """Test to make sure the words are accurately tagged.
+        """
         # Make sure the words are tagged.
         for sent in self.result:
             for tw in sent.tagged:
                 self.failIf(tw.tag == "")
-        
+
 class ParagraphTests(CommonTests, unittest.TestCase):
+    """Tests using a paragraph of text.
+    """
     def setUp(self):
+        """Set up the local variables.
+        """
         example = "She should have died hereafter; There would have " \
             + "been a time for such a word. Tomorrow, and tomorrow, and " \
             + "tomorrow, Creeps in this petty pace from day to day, To the " \
@@ -35,28 +49,36 @@ class ParagraphTests(CommonTests, unittest.TestCase):
             + "frets his hour upon the stage And then is heard no more. It " \
             + "is a tale Told by an idiot, full of sound and fury Signifying " \
             + "nothing."
-        super(ParagraphTests, self).setUp(text = example)
+        super(ParagraphTests, self).setUp(text=example)
 
     def test_sentences(self):
-        # Make sure it's a list of all the sentences.
+        """Make sure it's a list of all the sentences.
+        """
         # Todo: Check without hardcoding the ends?
-        for sent in self.result: 
+        for sent in self.result:
             self.failUnless(isinstance(sent, sentence.Sentence))
             self.failUnless(sent.tagged[-2].word[0] in ["word", "death",
                 "candle", "more", "nothing"])
 
 class SentenceTests(CommonTests, unittest.TestCase):
+    """Test given a single sentence.
+    """
     def setUp(self):
+        """Set up the example text.
+        """
         example = "The quick brown fox jumped over the lazy dog."
-        super(SentenceTests, self).setUp(text = example)
-        
+        super(SentenceTests, self).setUp(text=example)
+
     def test_sentences(self):
-        # Make sure it's a list of sentences.
-        for sent in self.result: 
-            self.failUnless(isinstance(sent, sentence.Sentence))
+        """Make sure it's a list of the given sentence.
+        """
+        self.failUnless(len(self.result) == 1)
+        self.failUnless(isinstance(self.result[0], sentence.Sentence))
+        self.failUnless(self.result[0].text == self.example)
 
     def test_space_before(self):
-        # Make sure space_before has been properly done
+        """Make sure space_before has been properly done
+        """
         for s in range(0, len(self.result)):
             for w in range(0, len(self.result[s].words)):
                 space = self.result[s].tagged[w].space_before
