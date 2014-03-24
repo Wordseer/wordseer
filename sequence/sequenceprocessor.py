f67dc3b5638f229a7e79de1a458bf4da01635d0c
PlasmaSheep <plasmasheep@gmail.com>
2014-03-23 21:17:14 -0700

    Faster exit for get_sequence

--- sequence/sequenceprocessor.py
+++ sequence/sequenceprocessor.py
@@ -96,6 +96,12 @@ class SequenceProcessor(object):
         return without_stops
 
     def process(self, sentence):
+        """Iterate and record every five word long sequence. The method records
+        using the ReaderWriter a list of sequences present in the given
+        sentence.
+        :param Sentence sentence: The sentence to process,
+        :return boolean: True.
+        """
         #TODO: implement timing?
         sequences = [] # a list of Sequences
         #TODO: there must be a way to make this nicer
@@ -122,69 +128,71 @@ class SequenceProcessor(object):
         sequences = []
         
         wordlist = sentence.tagged[i:j]
-        
         lemmatized_phrase = self.join_lemmas(wordlist, " ")
         surface_phrase = self.join_words(wordlist, " ")
+
+        if surface_phrase in self.previously_indexed:
+            #If we've already seen this sentence, don't bother
+            return sequences
         
-        words_nostops = self.remove_stops(wordlist)
-        lemmatized_phrase_nostops = self.join_lemmas(words_nostops,
+        wordlist_nostops = self.remove_stops(wordlist)
+        lemmatized_phrase_nostops = self.join_lemmas(wordlist_nostops,
             " ")
-        surface_phrase_nostops = self.join_words(words_nostops, " ")
+        surface_phrase_nostops = self.join_words(wordlist_nostops, " ")
 
         #TODO: these assignments could maybe be done better
-        has_stops = len(words_nostops) < len(wordlist)
+        has_stops = len(wordlist_nostops) < len(wordlist)
         lemmatized_has_stops = len(lemmatized_phrase_nostop)
         
-        all_stop_words = len(words_nostops) == 0
+        all_stop_words = len(wordlist_nostops) == 0
         lemmatized_all_stop_words = len(lemmatized_phrase_nostops) == 0
 
-        if not surface_phrase in self.previously_indexed:
-            sequences.append(Sequence(start_position=i,
+        sequences.append(Sequence(start_position=i,
+            sentence_id=sentence.id,
+            document_id=sentence.document_id,
+            sequence=surface_phrase,
+            is_lemmatized=False,
+            has_function_words=has_stops,
+            all_function_words=all_stop_words,
+            words=wordlist))
+        self.previously_indexed.append(surface_phrase)
+        
+        if (has_stops and not
+            all_stop_words and
+            wordlist_nostops[0] == wordlist[0] and not
+            surface_phrase_nostop in self.previously_indexed):
+                sequences.append(Sequence(start_position=i,
                 sentence_id=sentence.id,
                 document_id=sentence.document_id,
-                sequence=surface_phrase,
+                sequence=surface_phrase_nostop,
                 is_lemmatized=False,
-                has_function_words=has_stops,
-                all_function_words=all_stop_words,
-                words=wordlist))
-            self.previously_indexed.append(surface_phrase)
+                has_function_words=False,
+                all_function_words=False,
+                words=words_nostop))
+
+        sequences.append(Sequence(start_position=i,
+            sentence_id=sentence.id,
+            document_id=sentence.document_id,
+            sequence=lemmatized_phrase,
+            is_lemmatized=True,
+            has_function_words=lemmatized_has_stops,
+            all_function_words=lemmatized_all_stop_words,
+            words=wordlist))
             
-            if (has_stops and not
-                all_stop_words and
-                words_nostops[0] == wordlist[0] and not
-                surface_phrase_nostop in self.previously_indexed):
-                    sequences.append(Sequence(start_position=i,
+        self.previously_indexed.append(lemmatized_phrase)
+        
+        if (not lemmatized_phrase_nostops in self.previously_indexed and
+            lemmatized_has_stops and not
+            lemmatized_all_stop_words and
+            words_without_stops[0] == words[0]):
+                sequences.append(Sequence(start_position=i,
                     sentence_id=sentence.id,
                     document_id=sentence.document_id,
-                    sequence=surface_phrase_nostop,
-                    is_lemmatized=False,
+                    sequence=lemmatized_phrase_mostops,
+                    is_lemmatized=True,
                     has_function_words=False,
                     all_function_words=False,
-                    words=words_nostop))
-
-            sequences.append(Sequence(start_position=i,
-                sentence_id=sentence.id,
-                document_id=sentence.document_id,
-                sequence=lemmatized_phrase,
-                is_lemmatized=True,
-                has_function_words=lemmatized_has_stops,
-                all_function_words=lemmatized_all_stop_words,
-                words=wordlist))
-                
-            self.previously_indexed.append(lemmatized_phrase)
-            
-            if (not lemmatized_phrase_nostops in self.previously_indexed and
-                lemmatized_has_stops and not
-                lemmatized_all_stop_words and
-                words_without_stops[0] == words[0]):
-                    sequences.append(Sequence(start_position=i,
-                        sentence_id=sentence.id,
-                        document_id=sentence.document_id,
-                        sequence=lemmatized_phrase_mostops,
-                        is_lemmatized=True,
-                        has_function_words=False,
-                        all_function_words=False,
-                        words=words_nostops))
+                    words=wordlist_nostops))
 
         return sequences
 
