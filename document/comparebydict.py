602c3b4c0dac67ca48072d2269c3bdba959c06a8
PlasmaSheep <plasmasheep@gmail.com>
2014-03-26 22:30:30 -0700

    Documented comparebydict

--- document/comparebydict.py
+++ document/comparebydict.py
@@ -1,4 +1,12 @@
+"""
+A mixin for classes all compared the same way.
+"""
+
 class CompareByDict(object):
+    """A mixin for classes that should be compared by their attribute/value
+    pairs. Useful for the Document classes.
+    """
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
