f3bacd6397d93171c005710d0812d59f76583235
PlasmaSheep <plasmasheep@gmail.com>
2014-03-29 21:59:16 -0700

    Documentation for parseproducts.py

--- parser/parseproducts.py
+++ parser/parseproducts.py
@@ -1,10 +1,17 @@
-# TODO: real documentation
+# TODO: what is syntactic_parse?
 class ParseProducts(object):
+    """This class is a container for the results of Parser.parse. It contains
+    a syntactic_parse(?), a list of the dependencies in the parsed sentence,
+    and a list of tagged words in the parsed sentence.
+    """
+
     def __init__(self, syntactic_parse, dependencies, pos_tags):
         """
         :param string syntactic_parse:
-        :param list dependencies: A list of Dependencies
-        :param list pos_tags: A list of TaggedWords
+        :param list dependencies: A list of Dependencies that were present
+        in the parsed sentence.
+        :param list pos_tags: A list of TaggedWords that were present in the
+        parsed sentence.
         """
 
         self.syntactic_parse = syntactic_parse
