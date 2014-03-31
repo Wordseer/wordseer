58416c6545906f23f081a86963a2659c088a0838
PlasmaSheep <plasmasheep@gmail.com>
2014-03-31 10:59:12 -0700

    Better multi line string

--- tests/teststringprocessor.py
+++ tests/teststringprocessor.py
@@ -40,15 +40,15 @@ class ParagraphTests(CommonTests, unittest.TestCase):
     def setUp(self):
         """Set up the local variables.
         """
-        example = "She should have died hereafter; There would have " \
-            + "been a time for such a word. Tomorrow, and tomorrow, and " \
-            + "tomorrow, Creeps in this petty pace from day to day, To the " \
-            + "last syllable of recorded time; And all our yesterdays have " \
-            + "lighted fools The way to dusty death. Out, out, brief candle! " \
-            + "Life's but a walking shadow, a poor player That struts and " \
-            + "frets his hour upon the stage And then is heard no more. It " \
-            + "is a tale Told by an idiot, full of sound and fury Signifying " \
-            + "nothing."
+        example = ("She should have died hereafter; There would have "
+            "been a time for such a word. Tomorrow, and tomorrow, and "
+            "tomorrow, Creeps in this petty pace from day to day, To the "
+            "last syllable of recorded time; And all our yesterdays have "
+            "lighted fools The way to dusty death. Out, out, brief candle! "
+            "Life's but a walking shadow, a poor player That struts and "
+            "frets his hour upon the stage And then is heard no more. It "
+            "is a tale Told by an idiot, full of sound and fury Signifying "
+            "nothing.")
         super(ParagraphTests, self).setUp(text=example)
 
     def test_sentences(self):
