79dd23883b4a792d238b9057f660b9c600f62217
PlasmaSheep <plasmasheep@gmail.com>
2014-03-30 18:36:16 -0700

    Cleaned up parts of collectionprocessor

--- collectionprocessor.py
+++ collectionprocessor.py
@@ -1,3 +1,8 @@
+"""
+This file has tools to process a collection of files and contains the command
+line interface to the wordseer backend.
+"""
+
 import argparse
 import config
 import database
@@ -6,24 +11,34 @@ import os
 from structureextractor import StructureExtractor
 from stringprocessor import StringProcessor
 
+#TODO: are dbname, username, password even necessary?
+
 class CollectionProcessor(object):
-    def process(self, collection_dir, document_structure_file_name,
+    """Process a collection of files.
+    """
+    def process(self, collection_dir, docstruc_file_name,
         file_name_extension, dbname, username, password, start_from_scratch):
         """
-        This function:
+        This function relies on several subfunctions to:
         1. Sets up the database if necessary
         2. Extracts metadata, populates the narratives, sentenes, and paragraphs
         tables
         3. Processes the sentences by tokenizing and indexing the words
         4. Processes the sentences by performing grammatical parsing
-        Arguments:
-        start_from_scratch: If true, then the tables in the database will be
-        recreated.
+
+        :param str collection_dir: The directory whose files should be
+        processed.
+        :param str docstructure_file_name: The name of the JSON file that
+        describes the structure in the document files.
+        :param str file_name_extension: Files with this extension will be parsed
+        as documents.
+        :param boolean start_from_scratch: If true, then the tables in the
+        database will be recreated.
         """
         # Set up database if necessary
         if start_from_scratch is True:
-            with database.Database() as db:
-                db.reset()
+            with database.Database() as database:
+                database.reset()
 
         # TODO: MySQLDataReaderWriter
         #reader_writer = MySQLDataReaderWriter(db, (grammatial_processing or
@@ -35,68 +50,71 @@ class CollectionProcessor(object):
         # tables
         if not "true" in logger.get("finished_recording_text_and_metadata"):
             print("Extracting document text and metadata")
-            self.extract_record_metadata(str_proc,
-                document_structure_file_name, collection_dir)
+            self.extract_record_metadata(str_proc, collection_dir,
+                docstruc_file_name, filename_extension)
 
         # Parse the documents
-        if config.GRAMMATICAL_PROCESSING or (config.WORD_TO_WORD_SIMILARITY and
-            config.PART_OF_SPEECH_TAGGING):
+        if (config.GRAMMATICAL_PROCESSING or (config.WORD_TO_WORD_SIMILARITY and
+            config.PART_OF_SPEECH_TAGGING) and not
+            "true" in logger.get("finished_grammatical_processing").lower()):
             print("Parsing documents")
-            if not "true" in logger.get("finished_grammatical_processing").lower()
+            self.parse_documents()
 
-    def extract_record_metadata(self, str_proc, document_structure_file_name,
-        collection_dir):
-        """Extract metadata from each file in collection_dir, and populate the
-        documents, sentences, and document structure database tables.
+def extract_record_metadata(str_proc, collection_dir, docstruc_file_name,
+    filename_extension):
+    """Extract metadata from each file in collection_dir, and populate the
+    documents, sentences, and document structure database tables.
 
-        :param StringProcessor str_proc: An instance of StringProcessor
-        :param str document_structure_file_name: A JSON description of the
-        document structure.
-        :param str collection_dir: The directory from which files should be
-        parsed.
-        """
-        extractor = StructureExtractor(str_proc,
-            document_structure_file_name)
+    :param StringProcessor str_proc: An instance of StringProcessor
+    :param str collection_dir: The directory from which files should be
+    parsed.
+    :param str docstruc_file_name: A JSON description of the
+    document structure.
+    :param str filename_extension: The extension of the files that contain
+    documents.
+    """
+    extractor = StructureExtractor(str_proc, docstruc_file_name)
 
-        # Extract and record metadata, text for documents in the collection
-        num_files_done = 1
-        contents = []
-        for filename in os.listdir(collection_dir):
-            if (os.path.splitext(filename)[1].lower() ==
-                file_name_extension.lower()):
-                contents.append(filename)
+    # Extract and record metadata, text for documents in the collection
+    num_files_done = 1
+    contents = []
+    for filename in os.listdir(collection_dir):
+        if (os.path.splitext(filename)[1].lower() ==
+            file_name_extension.lower()):
+            contents.append(filename)
 
-        docs = [] # list of Documents
+    docs = [] # list of Documents
 
-        for filename in contents:
-            if (not "[" + num_files_done + "]" in
-                logger.get("text_and_metadata_recorded") and not
-                filename[0] == "."):
-                logger.log("finished_recording_text_and_metadata", "false",
-                    logger.REPLACE)
-                try:
-                    docs = extractor.extract(filename)
-                    for doc in docs:
-                        # TODO: readerwriter
-                        #reader_writer.create_new_document(doc,
-                        #   num_files_done)
-                        pass
+    for filename in contents:
+        if (not "[" + num_files_done + "]" in
+            logger.get("text_and_metadata_recorded") and not
+            filename[0] == "."):
+            logger.log("finished_recording_text_and_metadata", "false",
+                logger.REPLACE)
+            try:
+                docs = extractor.extract(filename)
+                for doc in docs:
+                    # TODO: readerwriter
+                    #reader_writer.create_new_document(doc,
+                    #   num_files_done)
+                    pass
 
-                    print("\t" + num_files_done + "/" + str(len(contents)) +
-                        "\t" + filename)
-                    logger.log("text_and_metadata_recorded",
-                        num_files_done + "", logger.UPDATE)
+                print("\t" + num_files_done + "/" + str(len(contents)) +
+                    "\t" + filename)
+                logger.log("text_and_metadata_recorded",
+                    num_files_done + "", logger.UPDATE)
 
-                except Exception as e:
-                    print("Error on file " + filename)
-                    print(e)
+            except Exception as e:
+                print("Error on file " + filename)
+                print(e)
 
-            num_files_done += 1
+        num_files_done += 1
 
-        logger.log("finished_recording_text_and_metadata", "true",
-            "replace")
+    logger.log("finished_recording_text_and_metadata", "true",
+        "replace")
 
-
+    def parse_documents(self):
+        pass
 
 def main(argv):
     """This is the root method of the pipeline, this is where the user
