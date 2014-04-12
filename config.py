43f3342ad549458348914f53054d7dd3a13e3b9d
PlasmaSheep <plasmasheep@gmail.com>
2014-04-11 20:53:14 -0700

    Updated config to work with csrf

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-app_name = os.path.basename(basedir) # assuming application name is same as folder
+# assuming application name is same as folder
+app_name = os.path.basename(basedir) 
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, app_name + ".db")
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db')
+
+CSRF_ENABLED = True
+SECRET_KEY = "secret" #TODO: change this in production
