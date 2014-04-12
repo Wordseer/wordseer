8c29950e792470dda20bd5cb9c861c607780594a
PlasmaSheep <plasmasheep@gmail.com>
2014-04-12 10:32:01 -0700

    Placeholder project models

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -1,6 +1,15 @@
 from app import app
+from .. import models
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route("/projects")
+def projects_list():
+    pass
+
+@app.route("/projects/new")
+def projects_new():
+    pass
