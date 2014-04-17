0c09fd110fbc18e876e16813a494974e0348e26b
PlasmaSheep <plasmasheep@gmail.com>
2014-04-16 19:36:22 -0700

    Added path splitting filter to jinja

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
+import os
+
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
 
 app.config.from_object('config')
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 CsrfProtect(app)
+
+def get_filename(string):
+    """
+    Given a path, return the name of the file.
+
+    Used in templates that list filenames.
+
+    :param str string: The path to split.
+    """
+    return os.path.split(string)[1]
+
+app.jinja_env.filters['get_filename'] = get_filename
 
 from app.controllers import *
 from app.models import *
