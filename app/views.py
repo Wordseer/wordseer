a1f29e1e04763f0b4a0842de2b3f77b413120220
PlasmaSheep <plasmasheep@gmail.com>
2014-04-18 16:16:23 -0700

    Removed a bunch of old code

--- app/views.py
+++ app/views.py
@@ -36,7 +36,7 @@ def project_show(project_id):
     Show the files contained in a specific project. It also allows the user
     to upload a new document, much like projects().
 
-    :param int proj_id: The ID of the desired project.
+    :param int project_id: The ID of the desired project.
     """
 
     upload_form = forms.DocumentUploadForm(prefix="upload")
@@ -49,7 +49,7 @@ def project_show(project_id):
             dest_path = os.path.join(app.config["UPLOAD_DIR"],
                 filename)
             uploaded_file.save(dest_path)
-            unit = Unit(path=dest_path, project=proj_id)
+            unit = Unit(path=dest_path, project=project_id)
             unit.save()
         #TODO: check other form as well
 
@@ -64,27 +64,9 @@ def project_show(project_id):
     return render_template("document_list.html", files=file_info,
         project=project, upload_form=upload_form, process_form=process_form)
 
-#def allowed_file(filename):
-    #return os.path.splitext(filename)[1] in app.config["ALLOWED_EXTENSIONS"]
 
-#@app.route(DOCUMENT_ROUTE)
-#def document_index():
-    #"""
-    #The index action, which shows all files for a user.
-
-    #Eventually, this will be swapped out for the more useful page that shows
-    #all files in a collection.
-    #return render_template("documents_list.html")
-    #"""
-    #file_list = []
-    #for unit in models.Unit.all().all():
-        #if unit.path:
-            #file_list.append(unit)
-    ##TODO: these paths are ugly
-    #return render_template("document_index.html", files=file_list)
-
-@app.route(DOCUMENT_ROUTE + '<document_id>')
-def document_show(document_id):
+@app.route(PROJECT_ROUTE + "<project_id>" + DOCUMENT_ROUTE + '<document_id>')
+def document_show(project_id, document_id):
     """
     The show action, which shows details for a particular document.
 
@@ -92,30 +74,8 @@ def document_show(document_id):
     """
     return render_template("document_show.html")
 
-#@app.route(DOCUMENT_ROUTE + 'new', methods=["GET", "POST"])
-#def document_upload():
-    #"""
-    #The new action for documents, which shows a form for uploading
-    #document files to process.
-    #"""
-
-    #if request.method == "POST":
-        #uploaded_file = request.files["uploaded_file"]
-        #if uploaded_file and allowed_file(uploaded_file.filename):
-            #filename = secure_filename(uploaded_file.filename)
-            #dest_path = os.path.join(app.config["UPLOAD_DIR"],
-                #filename)
-            #uploaded_file.save(dest_path)
-            ##TODO: send the user somewhere useful?
-            #unit = models.Unit(path=dest_path)
-            #unit.save()
-    
-    #form = forms.DocumentUploadForm()
-    
-    #return render_template('document_upload.html', form=form)
-
-@app.route(DOCUMENT_ROUTE + 'create/')
-def document_create():
+@app.route(PROJECT_ROUTE + "<project_id>" + DOCUMENT_ROUTE + 'create/')
+def document_create(project_id):
     """
     The create action for documents, which takes in document files, processes
     them, and creates the corresponding database records.
