f85d4ed65668d5a49c76f1df1551490d56534661
PlasmaSheep <plasmasheep@gmail.com>
2014-04-26 19:58:33 -0700

    Documentation for click-anywhere.js

--- app/static/click-anywhere.js
+++ app/static/click-anywhere.js
@@ -1,7 +1,12 @@
+// Given a table in which the rows have checkboxes, allow the user to click
+// anywhere on the row to activate the checkbox.
+
 $('.click-anywhere tr').click(function (e) {
     if(!$(e.target).is('td input:checkbox') && !$(e.target).is('th') )
     $(this).find('input:checkbox').trigger('click');
 });
+
+// This function makes sure that the user can click on links in the trs.
 
 $('.click-anywhere a').click(function (e) {
     e.stopPropagation();
