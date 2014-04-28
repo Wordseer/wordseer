877da55f45636a934cf1a1218a90f668e3aab477
PlasmaSheep <plasmasheep@gmail.com>
2014-04-27 20:42:40 -0700

    Cleaned up runtests.py

--- runtests.py
+++ runtests.py
@@ -6,8 +6,5 @@ os.environ['FLASK_ENV'] = "testing"
 
 import tests
 
-print "blah"
-print os.environ['FLASK_ENV']
-
 suite = unittest.TestLoader().loadTestsFromModule(tests)
 results = unittest.TextTestRunner(verbosity=2).run(suite)
