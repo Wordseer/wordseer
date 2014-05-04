45675db682f5c24b892626f9979fa573f6d03947
PlasmaSheep <plasmasheep@gmail.com>
2014-05-03 19:24:01 -0700

    Buttons now support glyphicons from bootstrap

--- app/widgets.py
+++ app/widgets.py
@@ -15,7 +15,12 @@ class ButtonWidget(object):
         if field.value is not None:
             kwargs.setdefault('value', field.value)
         kwargs.setdefault('type', "submit")
-        return HTMLString('<button %s>%s</button>' % (
+        icon = kwargs.pop("icon", None)
+        icon_span = ""
+        if icon is not None:
+            icon_span = "<span class='glyphicon " + icon + " '></span> "
+        return HTMLString('<button %s>%s%s</button>' % (
             html_params(**kwargs),
+            icon_span,
             escape(field._value())
             ))
