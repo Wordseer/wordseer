6561cc4b7d586fb424fa149c85789d7645339ab1
PlasmaSheep <plasmasheep@gmail.com>
2014-05-05 18:29:38 -0700

    Fixed template issues and moved js to own file

---
+++ app/static/upload-input-group.js
@@ -0,0 +1,26 @@
+// Adds support for an input-group of a file select button and a field to
+// write the currently selected file to.
+// Requires the span to have a class of btn-file.
+
+$(document)
+    .on('change', '.btn-file :file', function() {
+        var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+        
+    });
+});
