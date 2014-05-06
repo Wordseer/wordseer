6f806e5870c68b962d67ae8c9cbb8fc7f97f9d64
PlasmaSheep <plasmasheep@gmail.com>
2014-05-05 17:48:04 -0700

    Documentation for macros and js

--- app/static/modal_on_delete.js
+++ app/static/modal_on_delete.js
@@ -1,3 +1,6 @@
+//Call up a modal to confirm pressing a certain button. This is not yet
+//functional.
+
 $('button.confirm-delete').on('click', function(e){
     console.log("foo")
     var $form=$(this).closest('form');
