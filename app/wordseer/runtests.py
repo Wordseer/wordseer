12470117238c4e011448bdf6f176c87c32db6a5b
PlasmaSheep <plasmasheep@gmail.com>
2014-05-20 19:13:23 -0700

    Removed unnecessary files

--- app/wordseer/runtests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys, os, unittest
-
-if __name__ == "__main__":
-    sys.path.insert(0, os.path.dirname(__file__))
-
-    suite = unittest.TestLoader().discover('tests')
-    results = unittest.TextTestRunner(verbosity=2).run(suite)
