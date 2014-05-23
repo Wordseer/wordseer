d0b5be62a67400512e25be82b783d69b155e3636
PlasmaSheep <plasmasheep@gmail.com>
2014-05-22 18:15:52 -0700

    Static files for wordseer bp working

--- app/wordseer/__init__.py
+++ app/wordseer/__init__.py
@@ -1,9 +1,13 @@
-"""Initialize the WordSeer website.
+"""Blueprint for the WordSeer interface.
 """
+import os
 
 from flask import Blueprint
 
-wordseer = Blueprint('wordseer', __name__, template_folder='templates')
+wordseer = Blueprint('wordseer', __name__,
+    template_folder='templates',
+    static_folder="static",
+    static_url_path=os.path.dirname(__file__))
 
 from . import views
 from . import models
