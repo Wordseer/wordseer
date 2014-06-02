34234992dc5343317e925c427ebd658efcc04762
PlasmaSheep <plasmasheep@gmail.com>
2014-06-01 17:59:45 -0700

    Cleaner and better docstrings for getfrequentwords

--- app/wordseer/views/wordfrequencies.py
+++ app/wordseer/views/wordfrequencies.py
@@ -16,61 +16,70 @@ from ...uploader.models import word_in_sentence
 
 PAGE_SIZE = 100
 
-class WordFrequencies(View):
-    def dispatch_request(self):
-        """Dispatch the request to this page.
-        """
+wordseer.add_url_rule('/word-frequencies/get-frequent-words',
+    view_func=WordFrequencies.as_view("word_frequencies"))
 
-        words = request.args.get("words")
-        page = request.args.get("page")
+@wordseer.route("/word-frequencies/get-frequent-words")
+def get_frequent_words(self):
+    """Use the functions in this file to return a JSON response.
 
-        if page:
-            results = self.get_word_frequencies(words.replace("*", "%"), page)
-            return jsonify(results)
+    Arguments:
+        words (str): The words to retrieve frequencies for, separated by commas.
+        page (str): Which page of results to retrieve (see
+            ``get_word_frequencies`` documentation).
 
-        else:
-            abort(400)
+    Returns:
+        If the request is successful, then it returns a JSON response as a
+        string. This response has the following structure::
 
-    def get_word_frequencies(self, words, page):
-        """Get the frequencies of the given words, returning the given page
-        of the pagination.
+            [
+                {
+                    "id": int,
+                    "word": str,
+                    "pos": int,
+                    "sentence_count": int
+                }
+            ]
 
-        Arguments:
-            words (string): A string of comma separated words (TODO: why not
-                make this a list?
-            page (int): This function automatically paginates the result
-                of the database query. Each page contains ``PAGE_SIZE`` number
-                of entries, a config variable set at the top of this file.
+        Further details on the return value can be found in the
+        ``get_word_frequencies`` documentation.
 
-        Retruns:
-            list: A list in which every item is a dict which holds the id,
-                word, pos, and length of the sentences attributes of every
-                word retrieved from the database.
-        """
-        answer = []
-        offset = page * PAGE_SIZE
+        If the required arguments (listed above) are not supplied, then a 400
+        error is returned.
+    """
 
-        if words:
-            wordlist = words.split(",")
-            for word in wordlist:
-                result = db.session.query(Word,
-                    func.count(word_in_sentence.c.word_id).label("sentences")
-                ).join(word_in_sentence).\
-                    group_by(Word).\
-                    order_by("sentences").\
-                    limit(PAGE_SIZE).\
-                    offset(offset).\
-                    filter(Word.word.like(word)).\
-                    all()
+    try:
+        words = request.args["words"]
+        page = request.args["page"]
+    except KeyError:
+        abort(400)
 
-                for word in result:
-                    answer.append({
-                        "id": word.id,
-                        "word": word.word,
-                        "pos": word.pos,
-                        "sentence_count": len(word.sentences),
-                    })
-        else:
+    results = self.get_word_frequencies(words.replace("*", "%"), page)
+    return jsonify(results)
+
+
+def get_word_frequencies(self, words, page):
+    """Get the frequencies of the given words, returning the given page
+    of the pagination.
+
+    Arguments:
+        words (string): A string of comma separated words (TODO: why not
+            make this a list?
+        page (int): This function automatically paginates the result
+            of the database query. Each page contains ``PAGE_SIZE`` number
+            of entries, a config variable set at the top of this file.
+
+    Retruns:
+        list: A list in which every item is a dict which holds the id,
+            word, pos, and length of the sentences attributes of every
+            word retrieved from the database.
+    """
+    answer = []
+    offset = page * PAGE_SIZE
+
+    if words:
+        wordlist = words.split(",")
+        for word in wordlist:
             result = db.session.query(Word,
                 func.count(word_in_sentence.c.word_id).label("sentences")
             ).join(word_in_sentence).\
@@ -78,6 +87,7 @@ class WordFrequencies(View):
                 order_by("sentences").\
                 limit(PAGE_SIZE).\
                 offset(offset).\
+                filter(Word.word.like(word)).\
                 all()
 
             for word in result:
@@ -85,11 +95,25 @@ class WordFrequencies(View):
                     "id": word.id,
                     "word": word.word,
                     "pos": word.pos,
-                    "sentence_count": word.sentence_count,
+                    "sentence_count": len(word.sentences),
                 })
+    else:
+        result = db.session.query(Word,
+            func.count(word_in_sentence.c.word_id).label("sentences")
+        ).join(word_in_sentence).\
+            group_by(Word).\
+            order_by("sentences").\
+            limit(PAGE_SIZE).\
+            offset(offset).\
+            all()
 
-        return answer
+        for word in result:
+            answer.append({
+                "id": word.id,
+                "word": word.word,
+                "pos": word.pos,
+                "sentence_count": word.sentence_count,
+            })
 
-wordseer.add_url_rule('/word-frequencies/get-frequent-words',
-    view_func=WordFrequencies.as_view("word_frequencies"))
+    return answer
 
