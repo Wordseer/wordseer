a209337467783f2370d82cc2ae4e9630bab78822
PlasmaSheep <plasmasheep@gmail.com>
2014-06-02 21:35:33 -0700

    Further unit test refinements

--- app/wordseer/views/getpatterns.py
+++ app/wordseer/views/getpatterns.py
@@ -55,33 +55,33 @@ def get_patterns():
     end = int(request.args.get("end", -1))
     dependencies = []
 
-    if sentence and start > -1 and end > -1:
-        dependencies = db.session.query(Dependency).\
-            join(dependency_in_sentence).\
-            filter(Sentence.id == sentence).\
+    base_query = db.session.query(Dependency).\
+        join(dependency_in_sentence).\
+        filter(Sentence.id == sentence).\
+        filter(Dependency.relationship != "dep")
+
+    if start > -1 and end > -1:
+        dependencies = base_query.\
             filter(Dependency.gov_index <= end).\
             filter(Dependency.dep_index <= end).\
             filter(Dependency.gov_index >= start).\
             filter(Dependency.dep_index >= start).all()
 
-    elif sentence:
-        dependencies = db.session.query(Dependency).\
-            join(dependency_in_sentence).\
-            filter(Sentence.id == sentence).all()
+    else:
+        dependencies = base_query.all()
 
     information = []
 
     for dependency in dependencies:
-        if dependency.relationship != "dep":
-            #FIXME: mysterious fields, gov_id?
-            information.append({
-                "id": dependency.id,
-                "gov": dependency.governor,
-                #"gov_id": dependency.gov_id,
-                "dep": dependency.dependent,
-                #"dep_id": dependency.dep_id,
-                "relation": dependency.relationship
-            })
+        #FIXME: mysterious fields, gov_id?
+        information.append({
+            "id": dependency.id,
+            "gov": dependency.governor,
+            #"gov_id": dependency.gov_id,
+            "dep": dependency.dependent,
+            #"dep_id": dependency.dep_id,
+            "relation": dependency.relationship
+        })
 
     return jsonify(results=information)
 
