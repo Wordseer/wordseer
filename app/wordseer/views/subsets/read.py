0fbf7d88c6ab8251c904cd20e6e2df1dc5b83b7a
PlasmaSheep <plasmasheep@gmail.com>
2014-06-22 19:05:19 -0700

    Wrote get_highlight_text

--- app/wordseer/views/subsets/read.py
+++ app/wordseer/views/subsets/read.py
@@ -4,6 +4,7 @@ Returns the contents of subsets and lists all the subsets made by a user.
 from flask import jsonify
 
 from app.models import Set
+from app.models.association_objects import WordInSentence
 
 def list_subset_contents(set_id):
     """Return the contents of the ``Set`` with the given ID.
@@ -45,15 +46,48 @@ def list_subset_contents(set_id):
     return jsonify(contents)
 
 def get_highlight_text(start, start_index, end, end_index):
-    """???
+    """Return ``Words`` that meet the given criteria.
+
+    The query uses three conditionals to pick which ``Words`` to select:
+
+    1. If the ID of the ``Word``'s ``Sentence`` is greater than ``start`` and
+        less than ``end``
+    2. Or if the ID of the ``Word``'s ``Sentence`` is equal to ``start``
+        and the position of the ``Word`` is at least ``start_index``
+    3. Or if the ID of the ``Word``'s ``Sentence`` is equal to ``end`` and
+        the position of the ``Word`` is at most ``end_index``.
+
 
     Arguments:
-        start
-        start_index
-        end
-        end_index
+        start (int): The minimal sentence ID or the sentence ID.
+        start_index (int): The position of the word must be at least this.
+        end (int): The maximum sentence ID or the sentence ID.
+        end_index (int): Maximum position of the word.
+
+    Returns:
+        string: All the ``surface`` attributes from the matched ``Words`` put
+        together, separated by spaces; ``surface``s with punctuation in them
+        will not have a space before them.
     """
     #TODO: this has nothing to do with highlights, whatever those are
+    #TODO: this method is ridiculous, how is it usable? the arguments could
+    # mean two different things.
+    text = ""
+    words = WordInSentence.query.filter(
+        (WordInSentence.sentence_id > start) &
+            (WordInSentence.sentence_id < end) |
+        (WordInSentence.sentence_id == start) &
+            (WordInSentence.position >= start_index) |
+        (WordInSentence.sentence_id == end) &
+            (WordInSentence.position <= end_index)).\
+                order_by(WordInSentence.sentence_id).\
+                order_by(WordInSentence.position).all()
 
-    pass
+    for word in words:
+        if not app.config["PUNCTUATION_ALL"] in word.surface:
+            text += " " # Don't put spaces in front of punctuation.
+
+        text += word.surface
+
+    return text
 
