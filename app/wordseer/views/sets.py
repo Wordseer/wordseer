deba839512752cfa7e20d7381c94d768f33ffbe8
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-06-25 13:27:02 -0700

    "read" method from subsets/read.py

--- app/wordseer/views/sets.py
+++ app/wordseer/views/sets.py
@@ -14,33 +14,33 @@ from .. import helpers
 class CRUD(View):
     """CRUD Sets"""
     def __init__(self):
-        """initialize variables needed for Set operations
+        """initialize variables needed for ``Set`` operations
 
         request params:
         instance (str): ?
         type (str): Required
-        id (int): Required if type in ["read", "delete"]
-            or if type in ["update"] and update in ["add", "delete", "rename",
+        id (int): Required if ``type`` in ["read", "delete"]
+            or if ``type`` in ["update"] and ``update`` in ["add", "delete", "rename",
             "move","merge"]
-        collectiontype (str): Required if type in ["list", "listflat", "create"]
-        user (str): Required if type in ["list", "listflat", "create"]
-            or type in ["update"] and update in ["addNote", "addTag"]
-        name (str): Required if type in ["create"]
-        parent (?): Required if type in ["create"]
-        update (str): Required if type in ["update"]
-        item (int): Required if type in ["update"] and update in ["add",
+        collectiontype (str): Required if ``type`` in ["list", "listflat", "create"]
+        user (str): Required if ``type`` in ["list", "listflat", "create"]
+            or ``type`` in ["update"] and ``update`` in ["addNote", "addTag"]
+        name (str): Required if ``type`` in ["create"]
+        parent (?): Required if ``type`` in ["create"]
+        update (str): Required if ``type`` in ["update"]
+        item (int): Required if ``type`` in ["update"] and ``update`` in ["add",
             "delete", "addNote", "addTag", "deleteNote", "deleteTag"]
-        annotation (?): Required if type in ["update"] and update in ["addNote",
+        annotation (?): Required if ``type`` in ["update"] and ``update`` in ["addNote",
             "addTag"]
-        annotationID (int): Required if type in ["update"] and update in
+        annotationID (int): Required if ``type`` in ["update"] and ``update`` in
             ["deleteNote", "deleteTag"]
-        itemType (str): Required if type in ["update"] and update
+        itemType (str): Required if ``type`` in ["update"] and ``update``
             in ["addNote", "addTag"]
-        noteID (int): Required if type in ["update"] and update in ["editNote"]
-        text (str): Required if type in ["update"] and update in ["editNote"]
-        newName (str): Required if type in ["update"] and update in ["rename"]
-        newParent (?): Required if type in ["update"] and update in ["move"]
-        mergeInto (?): Required if type in ["update"] and update in ["merge"]
+        noteID (int): Required if ``type`` in ["update"] and ``update`` in ["editNote"]
+        text (str): Required if ``type`` in ["update"] and ``update`` in ["editNote"]
+        newName (str): Required if ``type`` in ["update"] and ``update`` in ["rename"]
+        newParent (?): Required if ``type`` in ["update"] and ``update`` in ["move"]
+        mergeInto (?): Required if ``type`` in ["update"] and ``update`` in ["merge"]
         """
         # required args
         try:
@@ -66,13 +66,52 @@ class CRUD(View):
         self.merge_into = request.args.get("mergeInto", None)
 
     def read(self):
-        """Returns the contents of the Set with the given ID"""
+        """Returns the contents of the Set with the given ID
+
+            Arguments:
+                subset_id (int): ID of the ``Set`` to list.
+
+            Returns:
+                list: Contents of the requested ``Set``, a dict with the following
+                fields:
+
+                - date: Creation date of the ``Set``
+                - text: Name of the ``Set``
+                - type: Type of the ``Set``
+                - id: ID of the ``Set``
+                - phrases: If this is a ``SequenceSet``, a list
+                    of phrases in this ``Set``.
+                - ids: If it's not a ``SequenceSet``, then a list of the item IDs in
+                    the ``Set``.
+            """
+            #TODO: why don't we just return a list of IDs in both cases?
+            #TODO: why do we need to return the ID?
+
+            contents = {}
+            requested_set = Set.query.get(set_id)
+
+            contents["text"] = requested_set.name
+            contents["id"] = requested_set.id
+            contents["date"] = requested_set.date
+            contents["type"] = requested_set.type
+
+            if requested_set.type == "sequenceset":
+                contents["phrases"] = [sequence.sequence for sequence in
+                    requested_set.sequences]
+
+            else:
+                contents["ids"] = [item.id for item in requested_set.get_items()]
+
+            return jsonify(contents)
+
         # check for required args
-        if self.id:
-            # MODEL METHOD: retrieve a set by ID
-            # php equivalent: subsets/read.php:listSubsetContents()
-        else:
-            abort(400)
+        # if self.id:
+        #     # MODEL METHOD: retrieve a set by ID
+        #     # php equivalent: subsets/read.php:listSubsetContents()
+        # else:
+        #     abort(400)
+
+
 
     # possible type values to dispatch
     types = dict()
