193eb8506246dfa9920029ffeafac7525de41604
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-06-30 17:35:27 -0700

    moved "create" over from old file

--- app/wordseer/views/sets.py
+++ app/wordseer/views/sets.py
@@ -219,11 +219,56 @@ class CRUD(View):
         else:
             abort(400)
 
+    def create(self):
+        """Create a new subset with the given information.
+
+        Arguments:
+            user (str): The username of the ``User`` that should own this ``Set``
+            set_name (str): The ``name`` for the set to be created.
+            set_parent: TODO: what is this and what does it do?
+            set_type (str): The tablename of set that should be created.
+
+        Returns:
+            On success, a JSON response like so::
+
+                {
+                    "status": "ok",
+                    "id": int, ID of the newly created Set
+                    "date": str, date of creation of this Set
+                }
+
+            On failure, a 400 error will be returned.
+        """
+
+        user = User.query.filter(User.name == username).one()
+
+        try:
+            set_model = get_model_from_tablename(set_type)(user=user,
+                name=set_name,
+                date=date.today()
+            )
+        except TypeError:
+            return abort(400)
+
+        set_model.save()
+
+        #TODO: updateMainMetadataCounts? what does that even do?
+        return jsonify(
+            {
+                "status": "ok", #TODO: let's get rid of this, 200 code is ok
+                "id": set_model.id,
+                "date": str(set_model.date)
+            }
+        )
+
+
     # possible type values to dispatch
-    operations = {}
-    operations["read"] = read
-    operations["list"] = list
-    operations["listflat"] = list_flat
+    operations = {
+        "read": read,
+        "list": list,
+        "listflat": list_flat,
+        "create": create,
+    }
 
     def dispatch_request(self):
         """choose function from dispatch table with key == ``request.type``
