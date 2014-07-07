79fb4b64f463fc5e60a03635972d45d6384005e6
jannah <hassan.jannah@gmail.com>
2014-07-07 04:38:52 -0700

    AWS deployment test - added WSGIPath

--- wordseer.py
+++ wordseer.py
@@ -10,11 +10,11 @@ if len(sys.argv)>1:
     hostname = sys.argv[1]
 if len(sys.argv)>2:
     portnum = int(float(sys.argv[2]))
-from app import app
+from app import app as application
 
 
 
 if __name__ == '__main__':
 #    app.debug = True
     
-    app.run(host=hostname, port=portnum, debug=True)
+    application.run(host=hostname, port=portnum, debug=True)
