4de36496e597cb32f1fa64639adb22fa055d7ff0
PlasmaSheep <plasmasheep@gmail.com>
2014-07-09 21:14:49 -0700

    Removed and moved files

--- runtests.py
+++ runtests.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     database.reset()
     database.cache()
 
+    #sys.path.insert(0, os.path.dirname(__file__))
     suite = unittest.TestLoader().discover('tests')
     results = unittest.TextTestRunner(verbosity=2).run(suite)
 
