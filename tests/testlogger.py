55639140ab0e05a2fe322b4718e920b7bdd9db9d
PlasmaSheep <plasmasheep@gmail.com>
2014-07-10 18:53:33 -0700

    Updated unit tests to fix last exception

--- tests/testlogger.py
+++ tests/testlogger.py
@@ -4,17 +4,18 @@ Tests for the Logger class.
 
 import unittest
 
+from app import db
+import database
 from lib.wordseerbackend.wordseerbackend import logger
-from lib.wordseerbackend.wordseerbackend.database import database
-from lib.wordseerbackend.wordseerbackend.models import Log
+from app.models.log import Log
 
 class LoggerTests(unittest.TestCase):
     """Run tests on the Logger class.
     """
     def setUp(self):
-        """Set up the Logger and the Database.
+        """Clean the database.
         """
-        self.db = database.Database()
+        database.restore_cache()
 
     def test_log(self):
         """Test the log() method. These tests assume that get() works.
@@ -29,11 +30,10 @@ class LoggerTests(unittest.TestCase):
         """Test the get() method.
         """
         entry = Log(item_value="true", log_item="logtest")
-        self.db.session.merge(entry)
-        self.db.session.commit()
+        db.session.merge(entry)
+        db.session.commit()
         self.failUnless(logger.get("logtest") ==
-            self.db.session.query(Log).filter(Log.log_item == "logtest").\
-            all()[0].item_value)
+            Log.query.filter(Log.log_item == "logtest").all()[0].item_value)
 
         self.failUnless(logger.get("fakerandomname") == "")
 
