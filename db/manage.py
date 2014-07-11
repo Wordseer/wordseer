d2862cb55b8143e4fe179ad13f907dbe7e915f89
jannah <hassan.jannah@gmail.com>
2014-07-10 22:14:35 -0700

    Added: save structure file back + bug fixes + folder restructuring

--- db/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from 'C:\Python27\Scripts\wordseer\lib\site-packages\six.pyc'>')
