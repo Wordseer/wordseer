282c1b5bec42442b9c65f7994c349ce4d340de25
jannah <hassan.jannah@gmail.com>
2014-07-16 18:01:41 -0700

    sync

---
+++ setup.py
@@ -0,0 +1,27 @@
+__author__="Hassan"
+__date__ ="$Jul 16, 2014 5:52:24 PM$"
+
+from setuptools import setup,find_packages
+
+setup (
+  name = 'wordseer_flask',
+  version = '0.1',
+  packages = find_packages(),
+
+  # Declare your packages' dependencies here, for eg:
+  install_requires=['foo>=3'],
+
+  # Fill in these to make your Egg ready for upload to
+  # PyPI
+  author = 'Hassan',
+  author_email = '',
+
+  summary = 'Just another Python package for the cheese shop',
+  url = '',
+  license = '',
+  long_description= 'Long description of the package',
+
+  # could also include long_description, download_url, classifiers, etc.
+
+  
+)
