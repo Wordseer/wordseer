8f9231a4617194b8cfde12b1240ffba98390336c
PlasmaSheep <plasmasheep@gmail.com>
2014-07-16 22:25:03 -0700

    Added corenlp to requirements, install.sh works

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+CORENLP=http://nlp.stanford.edu/software/stanford-corenlp-full-2013-06-20.zip
+REQUIREMENTS=requirements.txt
+CORENLP_DESTINATION="lib/wordseerbackend"
+CORENLP_FINAL_NAME="stanford-corenlp"
+
+DIRS=(${CORENLP//\// })
+FILENAME=${DIRS[${#DIRS[@]} - 1]}
+CORENLP_FINAL_PATH=$CORENLP_DESTINATION"/"$CORENLP_FINAL_NAME
+
+pip install -r $REQUIREMENTS
 
 python database.py create
-
 python database.py migrate
+
+cd $CORENLP_DESTINATION
+wget -O $CORENLP_FINAL_NAME".zip" $CORENLP
+unzip $CORENLP_FINAL_NAME".zip"
+mv ${FILENAME:0:-4} $CORENLP_FINAL_NAME
 
