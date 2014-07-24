6d602c8a53b15b3fbe41f53c450ebb6062c2d29d
PlasmaSheep <plasmasheep@gmail.com>
2014-07-23 21:19:35 -0700

    DocumentParser unit tests passing

--- lib/wordseerbackend/wordseerbackend/parser/documentparser.py
+++ lib/wordseerbackend/wordseerbackend/parser/documentparser.py
@@ -46,14 +46,14 @@ class DocumentParser(object):
 
         relationships = dict()
         dependencies = dict()
-        for sentence in document.all_sentences:
+        for sentence in document.sentences:
             if sentence.id > int(logger.get(LATEST_SENT_ID)):
-                parsed = self.parser.parse(sentence, relationships, dependencies)
+                parsed = self.parser.parse(sentence.text, relationships, dependencies)
                 products.append(parsed)
                 count += 1
                 current_max = sentence.id
 
-                if count % 50 == 0 or count == len(document.all_sentences):
+                if count % 50 == 0:
                     average_time = (datetime.now() - start_time).total_seconds()
                     print("Average parse speed after " + str(count) +
                         " sentences: " + str(average_time / count) +
@@ -63,25 +63,4 @@ class DocumentParser(object):
                     relationships = dict()
                     dependencies = dict()
                     db.session.commit()
-
-    def write_and_parse(self, products, current_max):
-        """Send a ParsedParagraph object to the ReaderWriter for writing, then
-        process each sentence returned from the write_parse_products with
-        SequenceProcessor.process(), then call ReaderWriter.write_sequences().
-        This method also updates the logs with the latest sentence ID and latest
-        sequenced sentence.
-
-        :param ParsedParagraph products: The ParsedParagraph to send
-        :param int current_max: The highest sentence ID yet processed.
-        """
-        #TODO: reader_writer
-        sentences = self.reader_writer.write_parse_products(products)
-
-        logger.log(LATEST_SENT_ID, str(current_max), logger.REPLACE)
-
-        for sentence in sentences:
-            self.sequence_processor.process(sentence)
-
-        logger.log(LATEST_SEQ_SENT, str(current_max), logger.REPLACE)
-        #TODO: reader_writer
-        self.reader_writer.write_sequences()
+        db.session.commit()
--- tests/testdocumentparser.py
+++ tests/testdocumentparser.py
@@ -2,6 +2,7 @@
 """
 import unittest
 from mock import MagicMock, patch, call, create_autospec
+import pdb
 
 from app.models.document import Document
 from app.models.sentence import Sentence
@@ -24,44 +25,8 @@ class DocumentParserTests(unittest.TestCase):
                 self.mock_reader_writer,
                 self.mock_str_proc)
 
-    def test_write_and_parse(self, mock_logger):
-        """Test the write_and_parse method.
-
-        This method patches logger.log
-        (to make sure that things are logged properly) as well as
-        SequenceProcessor. It mocks DataReaderWriter.
-        """
-
-        sentences = [Sentence(text="This is the first sentence"),
-            Sentence(text="This is the second sentence")]
-        current_max = 5
-        products = "products"
-
-        # Configure the readerwriter mock
-        attrs = {"write_parse_products.return_value": sentences}
-        self.mock_reader_writer.configure_mock(**attrs)
-
-        self.docparser.write_and_parse(products, current_max)
-
-        # write_parse_products should have been called with the products arg
-        self.mock_reader_writer.write_parse_products.\
-            assert_called_once_with(products)
-
-        # Logger should have been called twice with specific arguments
-        mock_logger.log.assert_any_call(documentparser.LATEST_SENT_ID,
-            str(current_max),
-            mock_logger.REPLACE)
-        mock_logger.log.assert_any_call(documentparser.LATEST_SEQ_SENT,
-            str(current_max),
-            mock_logger.REPLACE)
-
-        # The sequence processor should have been invoked for every sentence
-        self.docparser.sequence_processor.process.assert_any_call(sentences[0])
-        self.docparser.sequence_processor.process.assert_any_call(sentences[1])
-
-        self.mock_reader_writer.write_sequences.assert_called_once_with()
-
-    def test_parse_document(self, mock_logger):
+    @patch("lib.wordseerbackend.wordseerbackend.parser.documentparser.db", autospec=True)
+    def test_parse_document(self, mock_db, mock_logger):
         """Test the parse_document method.
 
         This method supplies a document of sixty sentences to make sure that
@@ -69,7 +34,7 @@ class DocumentParserTests(unittest.TestCase):
         parse_document().
         """
         latest_sent = 5
-        runs = 60
+        runs = 108
         # Mock out the write_and_parse method
         self.docparser.write_and_parse = MagicMock()
 
@@ -98,22 +63,9 @@ class DocumentParserTests(unittest.TestCase):
         # The parser should have only been called on sentences with an id > 5
         parse_calls = []
         for i in range(latest_sent + 1, runs):
-            parse_calls.append(call(mock_sents[i].text))
+            parse_calls.append(call(mock_sents[i].text, {}, {}))
         self.mock_str_proc.parse.assert_has_calls(parse_calls, any_order=True)
 
-        # Write and parse should have been called once for every block of 50
-        parsed = ParsedParagraph()
-        rw_calls = []
-        max_id = 0
-        for i in range(latest_sent + 1, runs):
-            parsed.add_sentence(mock_sents[i], mock_products)
-            max_id = i
-            if len(parsed.sentences) % 50 == 0:
-                rw_calls.append(call(parsed, i))
-                parsed = ParsedParagraph()
-
-        rw_calls.append(call(parsed, max_id))
-
-        self.docparser.write_and_parse.assert_has_calls(rw_calls,
-            any_order=True)
+        # Every 50 sentences we commit to the database
+        assert len(mock_db.session.commit.call_args_list) == (runs - latest_sent + 1) / 50 + 1
 
