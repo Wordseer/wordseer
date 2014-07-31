82eab0569a43547bc92795189441e8632e0e94cf
Keien Ohta <keienohta@gmail.com>
2014-07-31 04:51:52 +0000

    changed pipeline to r_and_j

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,7 +5,7 @@ import os
 import database
 import pdb
 
-collection_dir = os.path.join("tests", "data", "tweets")
+collection_dir = os.path.join("tests", "data", "r_and_j")
 extension = ".xml"
 structure_file = os.path.join(collection_dir, "structure.json")
 
