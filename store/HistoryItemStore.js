6cecae79a0bb9210beb761e69fb391ba3e2191c2
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-07-31 20:43:11 -0700

    history item autosync
    items in new panels weren’t getting added until the next history item
    was created … causing problems on reload of a panel view

--- store/HistoryItemStore.js
+++ store/HistoryItemStore.js
@@ -1,8 +1,8 @@
 /* Copyright 2012 Aditi Muralidharan. See the file "LICENSE" for the full license governing this code. */
 /** A central, shared store for keeping track of the user's search history. The
-storeId of this store is `HistoryItemStore`. There is always only one instance 
+storeId of this store is `HistoryItemStore`. There is always only one instance
 of this store. It can be accessed using `Ext.getStore("HistoryItemStore")`. It
-is instantiated and controlled by the 
+is instantiated and controlled by the
 {@link WordSeer.controller.HistoryController}.
 */
 Ext.define('WordSeer.store.HistoryItemStore', {
@@ -13,25 +13,26 @@ Ext.define('WordSeer.store.HistoryItemStore', {
 		history item.
 		*/
 		current: false,
+		autoSync: true,
 	},
 	proxy: {
 		type: 'localstorage',
 		id: 'HistoryItemStore',
 		writer: 'xml',
 		reader: 'xml',
-	}, 
+	},
 	model: 'WordSeer.model.HistoryItemModel',
 
-	/** Sets the most recent history item to the given 
+	/** Sets the most recent history item to the given
 	{@link WordSeer.model.HistoryItemModel HistoryItemModel} instance. Called
 	by the {@link WordSeer.controller.SearchController#newHistoryItem} method
 	when the user performs a new search or changes search parameters.
-	
+
 	@param {WordSeer.model.HistoryItemModel} history_item The history item
 	to set as the current one.
 	*/
 	setCurrent: function(history_item) {
-		if (history_item) {			
+		if (history_item) {
 			if (this.getCurrent()) {
 				this.getCurrent().set('is_current', false);
 			}
@@ -39,7 +40,7 @@ Ext.define('WordSeer.store.HistoryItemStore', {
 		}
 		this.current = history_item;
 	},
-	
+
 	/** Returns the most recent history item.
 	@return {WordSeer.model.HistoryItemModel} The most recent history item.
 	*/
