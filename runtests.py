0ab8556928c2c68fc8206acaa960f2e1ec36efb6
PlasmaSheep <plasmasheep@gmail.com>
2014-08-02 13:48:45 -0700

    Reworked runtests.py to use nose

--- runtests.py
+++ runtests.py
@@ -8,15 +8,23 @@ import os
 import sys
 import unittest
 import pdb
+from nose.core import main
 
 os.environ['FLASK_ENV'] = "testing"
 import database
 
-if __name__ == "__main__":
+def run_tests():
     database.reset()
     database.cache()
+    #sys.path.insert(0, os.path.dirname(__file__))
+    #suite = unittest.TestLoader().discover('tests')
+    #results = unittest.TextTestRunner(verbosity=2).run(suite)
+    results = main()
+    pdb.set_trace()
+    if results.wasSuccessful():
+        sys.exit(0)
+    sys.exit(1)
 
-    sys.path.insert(0, os.path.dirname(__file__))
-    suite = unittest.TestLoader().discover('tests')
-    results = unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == "__main__":
+    run_tests()
 
