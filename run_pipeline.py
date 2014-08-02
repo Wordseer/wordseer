bf76134af07e2eb8e7ea6a7d08b8f2ab98ca64e3
Keien Ohta <keienohta@gmail.com>
2014-08-02 19:20:03 +0000

    Added aaron slave narratives

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,7 +5,7 @@ import os
 import database
 import pdb
 
-collection_dir = os.path.join("tests", "data", "personals")
+collection_dir = os.path.join("tests", "data", "aaron")
 extension = ".xml"
 structure_file = os.path.join(collection_dir, "structure.json")
 
