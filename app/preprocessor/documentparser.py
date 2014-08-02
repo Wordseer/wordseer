add1dc88499df6dee6be148688054ed39e2d3dfc
PlasmaSheep <plasmasheep@gmail.com>
2014-08-01 21:36:44 -0700

    Debugging processing issues when processing from the front end

--- app/preprocessor/documentparser.py
+++ app/preprocessor/documentparser.py
@@ -36,6 +36,7 @@ class DocumentParser(object):
 
         :param Document document: The document to parse and record.
         """
+        pdb.set_trace()
 
         start_time = datetime.now()
         count = 0
