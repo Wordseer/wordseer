8721396ed6d320abee9f30d2863b6ee2d12d0039
PlasmaSheep <plasmasheep@gmail.com>
2014-08-07 15:41:59 -0700

    Some refactoring and development for sudo

--- install.py
+++ install.py
@@ -7,10 +7,13 @@ import pdb
 import subprocess
 import urllib2
 import zipfile
+import gzip
 
 # Config
 # Location of CoreNLP
 CORENLP = "http://nlp.stanford.edu/software/stanford-corenlp-full-2013-06-20.zip"
+# Location of libxml for macs
+LIBXML_MAC = "http://www.explain.com.au/download/combo-2007-10-07.dmg.gz"
 # Path to requirements file
 REQUIREMENTS = "requirements.txt"
 # Directory to save corenlp to
@@ -21,25 +24,40 @@ CORENLP_LOCAL_NAME = "stanford-corenlp"
 CORENLP_LOCAL_PATH = os.path.join(CORENLP_LOCAL_DIR, CORENLP_LOCAL_NAME)
 CORENLP_ZIP_DIRECTORY = os.path.splitext(os.path.basename(CORENLP))[0]
 
+def download_file(src, dest):
+    """Download a file using urllib2.
+    """
+    source_file = urllib2.urlopen(src)
+    with open(dest, "w") as local_file:
+        local_file.write(source_file.read())
+
+def install_prerequisites():
+    """Install requirements that we can't install in a virtual environment.
+    """
+    if "Linux" in system:
+        if "ARCH" in system:
+            subprocess.call(["sudo pacman -S libxslt libxml2 jre7-openjdk"])
+        if "Ubuntu" in system:
+            subprocess.call(["sudo apt-get install libxml2 libxslt1.1 openjdk-7-jre"])
+    if "Darwin" in system:
+        download_file(LIBXML_MAC, "libxml.dmg.gz")
+        with gzip.GzipFile("libxml.dmg.gz") as local_zip,
+            open("libxml.dmg", "w") as local_dmg:
+                local_dmg.write(local_zip.read())
+
 def main():
     """Perform the installation process.
     """
     parser = argparse.ArgumentParser(
         description="Install wordseer's requirements.")
-    root_method = parser.add_mutually_exclusive_group()
-    root_method.add_argument("--sudo", action="store_true",
+    parser.add_argument("--sudo", action="store_true",
         help="Use sudo to install some dependencies")
-    root_method.add_argument("--su", action="store_true",
-        help="Use su to become root to install some dependencies")
     args = parser.parse_args()
 
-    if args.sudo:
-        # Install things with sudo
-        pass
+    system = subprocess.check_output("uname -a")
 
-    elif args.su:
-        # Install things with su
-        pass
+    if args.sudo:
+        install_prerequisites()
 
     # Install things
     print "Installing python dependencies"
@@ -53,6 +71,7 @@ def main():
 
     print "Installing stanford-corenlp"
     source_file = urllib2.urlopen(CORENLP)
+
     with open("corenlp.zip", "w") as local_file:
         local_file.write(source_file.read())
 
