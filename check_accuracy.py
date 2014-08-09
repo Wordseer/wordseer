bed50a96d5bba675a663116a0dacb818381c9439
Keien Ohta <keienohta@gmail.com>
2014-08-09 01:43:41 +0000

    Finished accuracy check module

--- check_accuracy.py
+++ check_accuracy.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 import sys
 import os
 import random
+import difflib
+
+d = difflib.Differ()
 
 def check_documents(old_db, new_db):
 	""" Check accuracy of documents
@@ -43,38 +46,84 @@ def check_sentences(old_db, new_db):
 
 	ids_old = get_random_id_from("sentence", old_db, 20)
 
-	sentences_old = old_db.execute(
+	sentences_old = [ sentence for sentence in old_db.execute(
 		"""
 			SELECT *
 			FROM sentence
 			WHERE id in {ids}
 		""".format(ids = ids_old)
-	).fetchall()
+	).fetchall() ]
 
 	print("--- Check matching text ---")
 
+	sentences_old_texts = [ sentence.sentence.strip() for sentence in sentences_old ]
+
+	sentences_new = [ new_db.execute(
+		"""
+			SELECT *
+			FROM sentence
+			WHERE sentence.text = ?
+		""",
+		sentence
+	).fetchone() for sentence in sentences_old_texts ]
+
+	sentences_new_texts = [ sentence.text for sentence in sentences_new if sentence ]
+
+	for line in difflib.unified_diff(sentences_old_texts, sentences_new_texts, "old db", "new db"):
+		print("\t" + line[:64])
+
+	matches = len(sentences_new_texts)
+	print("\n\t" + str(matches) + " matches\n")
+
 	print("--- Check words ---")
+
+	sentence_ids_old = []
+	sentence_ids_new = []
+
+	for i in range(len(sentences_old)):
+		if sentences_new[i] != None:
+			sentence_ids_old.append(sentences_old[i].id)
+			sentence_ids_new.append(sentences_new[i].id)
 
 	words_old = [
 		old_db.execute(
 			"""
-				SELECT word.word as word, sentence_xref_word.position as position
+				SELECT word.word as word
 				FROM word INNER JOIN sentence_xref_word
 				ON word.id = sentence_xref_word.word_id
 				WHERE sentence_xref_word.sentence_id = {sentence_id}
+				ORDER BY sentence_xref_word.position
 			""".format(sentence_id=sentence_id)
 		).fetchall()
-		for sentence_id in ids_old
+		for sentence_id in sentence_ids_old
 	]
 
-	for words in words_old: 
-		print("\n")
-		sorted_words_old = [
-			word[0] for word in
-			sorted(words, key=lambda x: x[1])
-		]
+	words_new = [
+		new_db.execute(
+			"""
+				SELECT word.word as word
+				FROM word INNER JOIN word_in_sentence
+				ON word.id = word_in_sentence.word_id
+				WHERE word_in_sentence.sentence_id = {sentence_id}
+				ORDER BY word_in_sentence.position
+			""".format(sentence_id=sentence_id)
+		).fetchall()
+		for sentence_id in sentence_ids_new
+	]
 
-		print(sorted_words_old)
+	words_old = [ [ word.word for word in words] for words in words_old ]
+	words_new = [ [ word.word.lower() for word in words] for words in words_new ]
+
+	for i in range(0, len(words_old)):
+		diff = difflib.unified_diff(
+			words_old[i],
+			words_new[i],
+			" ".join(words_old[i])[:64],
+			" ".join(words_new[i])[:64]
+		)
+
+		for line in diff:
+			print("\t" + str(line))
 
 	print("--- Check dependencies ---")
 
@@ -91,10 +140,115 @@ def check_sentences(old_db, new_db):
 				WHERE dependency_xref_sentence.sentence_id = {sentence_id}
 			""".format(sentence_id=sentence_id)
 		).fetchall()
-		for sentence_id in ids_old
+		for sentence_id in sentence_ids_old
 	]
 
-	for dep in dependencies_old: print("\t"+str(dep))
+	for i in range(len(dependencies_old)):
+		for j in range(len(dependencies_old[i])):
+			governor = dependencies_old[i][j].governor.lower()
+			dependent = dependencies_old[i][j].dependent.lower()
+			relationship = dependencies_old[i][j].relationship.lower()
+
+			dependencies_old[i][j] = " ".join([governor, dependent, relationship])
+
+	dependencies_new = [
+		new_db.execute(
+			"""
+				SELECT
+					dependency.governor_id as governor,
+					dependency.dependent_id as dependent,
+					dependency.grammatical_relationship_id as relationship
+				FROM dependency
+					INNER JOIN dependency_in_sentence
+						ON dependency.id = dependency_in_sentence.dependency_id
+				WHERE dependency_in_sentence.sentence_id = {sentence_id}
+			""".format(sentence_id=sentence_id)
+		).fetchall()
+		for sentence_id in sentence_ids_new
+	]
+
+	for i in range(len(dependencies_new)):
+		for j in range(len(dependencies_new[i])):
+			governor_id = dependencies_new[i][j][0]
+			dependent_id = dependencies_new[i][j][1]
+			grammatical_relationship_id = dependencies_new[i][j][2]
+
+			governor = new_db.execute("""
+				SELECT word
+				FROM word
+				WHERE id = ?
+			""", governor_id).fetchone().word.lower()
+
+			dependent = new_db.execute("""
+				SELECT word
+				FROM word
+				WHERE id = ?
+			""", dependent_id).fetchone().word.lower()
+
+			grammatical_relationship = new_db.execute("""
+				SELECT name
+				FROM grammatical_relationship
+				WHERE id = ?
+			""", grammatical_relationship_id).fetchone().name.lower()
+
+			dependencies_new[i][j] = " ".join([governor, dependent, grammatical_relationship])
+
+	for i in range(len(dependencies_new)):
+		diff = difflib.unified_diff(
+			sorted(dependencies_old[i]),
+			sorted(dependencies_new[i]),
+		)
+
+		for line in diff:
+			print("\t" + str(line))
+
+	print("--- Check sequences ---")
+
+	sequences_old = [
+		old_db.execute("""
+			SELECT sequence.sequence
+			FROM sequence INNER JOIN sequence_xref_sentence
+			ON sequence.id = sequence_xref_sentence.sequence_id
+			WHERE sequence_xref_sentence.sentence_id = {sentence_id}
+			ORDER BY sequence.sequence
+		""".format(sentence_id=sentence_id)).fetchall()
+		for sentence_id in sentence_ids_old
+	]
+
+	sequences_old = [ set([ sequence.sequence.lower() for sequence in sequences ]) for sequences in sequences_old ]
+
+	sequences_new = [
+		new_db.execute("""
+			SELECT sequence.sequence
+			FROM sequence INNER JOIN sequence_in_sentence
+			ON sequence.id = sequence_in_sentence.sequence_id
+			WHERE sequence_in_sentence.sentence_id = {sentence_id}
+			ORDER BY sequence.sequence
+		""".format(sentence_id=sentence_id)).fetchall()
+		for sentence_id in sentence_ids_new
+	]
+
+	sequences_new = [ set([ sequence.sequence.lower() for sequence in sequences ]) for sequences in sequences_new ]
+
+	for i in range(len(sequences_old)):
+		print("\tSequences missing in new db:")
+		for item in sequences_old[i] - sequences_new[i]:
+			print("\t\t" + str(item))
+
+		print("\tSequences missing in old db:")
+		for item in sequences_new[i] - sequences_old[i]:
+			print("\t\t" + str(item))
+
+		print("\n")
+	# 	diff = difflib.unified_diff(
+	# 		sequences_old[i],
+	# 		sequences_new[i],
+	# 		" ".join(sequences_old[i]),
+	# 		" ".join(sequences_new[i]),
+	# 	)
+
+	# 	for line in diff:
+	# 		print("\t" + str(line))
 
 # Helpers
 
@@ -133,7 +287,7 @@ if __name__ == "__main__":
 
 	# Read the args and connect to the databases
 	old_db = create_engine("mysql://keien@localhost/" + sys.argv[1])
-	new_db = None # create_engine("sqlite://" + os.path.join(root, sys.argv[2]))
+	new_db = create_engine("sqlite:////" + os.path.join(root, sys.argv[2]))
 
 	# check_documents(old_db, new_db)
 	check_sentences(old_db, new_db)
