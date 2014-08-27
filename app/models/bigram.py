7712988228ee0269f32d77f2e759e8aacb68aa1a
PlasmaSheep <plasmasheep@gmail.com>
2014-08-27 12:41:19 -0700

    Added Bigram methods, fleshed out model

--- app/models/bigram.py
+++ app/models/bigram.py
@@ -4,6 +4,7 @@ sequences.
 
 from app import db
 from .base import Base
+from .association_objects import BigramOffset
 
 class Bigram(db.Model, Base):
     """A bigram consists of two words, one primary word and one secondary word.
@@ -11,6 +12,15 @@ class Bigram(db.Model, Base):
     Between them is some distance which is less than or equal to five words;
     the secondary word may be anywhere in relation to the primary word.
     """
+
+    word_id = db.Column(db.Integer, db.ForeignKey("word.id"))
+    secondary_word_id = db.Column(db.Integer, db.ForeignKey("word.id"))
+    frequency = db.Column(db.Integer)
+
+    word = db.relationship("Word", foreign_keys=word_id)
+    secondary_word = db.relationship("Word", foreign_keys=secondary_word_id)
+
+    offsets = db.relationship("BigramOffset", backref="bigram")
 
     def __init__(self, word, secondary_word):
         """Instantiate a bigram.
@@ -21,5 +31,39 @@ class Bigram(db.Model, Base):
         """
         self.word = word
         self.secondary_word = secondary_word
-        self.
+        self.offsets = [BigramOffset(offset=i, bigram=self)
+            for i in range(-5, 0) + range(1, 6)]
+
+    def get_offset(self, offset):
+        """Get the BigramOffset object that corresponds to a given offset
+        for this Bigram.
+
+        Arguments:
+            offset (int): The offset to get.
+
+        Returns:
+            BigramOffset: The correct BigramOffset object.
+        """
+        if offset < 0:
+            return self.offsets[offset + 5]
+        elif offset > 0:
+            return self.offsets[offset + 4]
+        else:
+            raise ValueError("Offset cannot be 0")
+
+    def add_instance(self, offset, sentence, force=True):
+        """Add an occurrence of this bigram.
+
+        Arguments:
+            offset (int): The offset of ``secondary_word`` from ``word``.
+            sentence (Sentence): The sentence where this instance ocurrs.
+
+        Returns:
+            BigramOffset: The modified BigramOffset object.
+        """
+        bigram_offset = self.get_offset(offset)
+        bigram_offset.sentences.append(sentence)
+        bigram_offset.save(force)
+
+        return bigram_offset
 
