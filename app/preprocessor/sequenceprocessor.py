6d2b99c9b2aae61ed97bdbdead6c73973ebc2edc
PlasmaSheep <plasmasheep@gmail.com>
2014-08-26 23:39:40 -0700

    Updated sequenceprocessor docstrings, working on adding bigrams

--- app/preprocessor/sequenceprocessor.py
+++ app/preprocessor/sequenceprocessor.py
@@ -32,20 +32,50 @@ class SequenceProcessor(object):
         self.project = project
         self.logger = logging.getLogger(__name__)
         self.project_logger = ProjectLogger(self.logger, project)
+        self.bigrams = {}
 
     def process(self, sentence, sequence_dict=None):
-        """Iterate and record every four word long sequence. The method records
-        using the ReaderWriter a list of sequences present in the given
-        sentence.
+        """Iterate and record every bigram present.
+
+        This function uses the xtract algorithm. In short, every lemma is a
+        member of a bigram along with every lemma that is less than five
+        words away from it in any given sentence.
+
+        This method iterates over every sentence in the project and then
+        iterates over every word in every sentence; each word and its sentence
+        is passed on to ``get_bigrams``.
 
         :param Sentence sentence: The sentence to process,
         :return list: A list of Sequence objects, representing the results
             of processing. These sequences are also sent to the ReaderWriter.
         """
-        bigrams = {}
-        for word in Word.query:
+        sentences = self.project.get_sentences()
+        for sentence in sentences:
+            for index, word in enumerate(sentence.words):
+                get_bigrams(sentence, word, index)
 
+    def get_bigrams(self, sentence, word, index):
+        """Handle the main bigram processing.
 
+        We use the xtract algorithm for getting bigrams. In short,
+        given a word, its index, and its sentence, we create bigrams of the
+        word's lemmas and lemmas that are +/- five words away.
+
+        If we encounter a lemma that already has a bigram (stored in
+        ``self.bigrams``, we increase its count.
+
+        Arguments:
+            sentence (Sentence): A sentence to get bigrams for.
+            word (Word): The word to use as the primary word for the bigrams.
+            index (int): The position of ``word`` in ``sentence``.
+        """
+        start_index = max(index - 5, 0)
+        end_index = min(index + 6, len(sentence.words))
+
+        for i in range(start_index, end_index):
+            lemma = sentence.words[i].lemma
+            if lemma in self.bigrams:
+                self.bigrams[lemma] = Bigram(word, lemma)
 
     def get_sequence(self, sentence, i, j):
         """Handle the main processing part in the process() loop.
