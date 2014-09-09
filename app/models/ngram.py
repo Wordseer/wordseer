dfd7acf9907b0f0513ad45e3b5bb3f2c989ce84f
PlasmaSheep <plasmasheep@gmail.com>
2014-09-08 19:15:34 -0700

    Ngram documentation.

--- app/models/ngram.py
+++ app/models/ngram.py
@@ -2,6 +2,16 @@ from app import db
 from .base import Base
 
 class Ngram(db.Model, Base):
+    """An Ngram is a collection of several words that co-occur with each other
+    unusually frequently.
+
+    Attributes:
+        count (int): How often this Ngram occurs in its corpus.
+        has_stop_words (boolean): If this Ngram contains stop words.
+        words (list of Words): Words that are present in this ngram.
+        text (string): The raw text of this ngram.
+    """
+
     count = db.Column(db.Integer)
     has_stop_words = db.Column(db.Boolean)
     words = db.relationship("Word",
