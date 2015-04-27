4ba2cf1c08616e95b77fe254ffb773aea100749b
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-27 14:59:47 -0700

    add missing dependency and database create/migrate
    fixes #236, fixes #237

--- install.py
+++ install.py
@@ -263,7 +263,8 @@ def setup_database():
     """
     print "Setting up database..."
     #TODO: Should we always call python2.7?
-    subprocess.call("python database.py reset", shell=True)
+    subprocess.call("python database.py create", shell=True)
+    subprocess.call("python database.py migrate", shell=True)
 
 def pip_is_installed():
     """Check if the correct version of pip is installed.
