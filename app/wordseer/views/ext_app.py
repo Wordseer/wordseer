4195aff33a6ba04921d3264d60e9f791bb3032a0
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-07-03 14:19:40 -0700

    include project name in page title

--- app/wordseer/views/ext_app.py
+++ app/wordseer/views/ext_app.py
@@ -2,10 +2,11 @@ from flask import render_template
 from flask_security.decorators import login_required
 
 from app import app
+from app.models.project import Project
 from .. import wordseer
 
 @wordseer.route(app.config["PROJECT_ROUTE"]+"<int:project_id>"+
     app.config["ANALYZE_ROUTE"])
 @login_required
 def ext_app(project_id):
-    return render_template('app.html', project_id=project_id)
+    return render_template('app.html', project_id=project_id, project_name=Project.query.get(project_id).name)
