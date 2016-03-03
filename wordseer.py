96a8f851d8018d8c949bae4e5d82500c7ee0494c
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2016-03-03 07:55:44 -0800

    stupid whitespace errors crashing the app

--- wordseer.py
+++ wordseer.py
@@ -2,14 +2,14 @@
 
 """Run the wordseer_flask website.
 """
-import os
 from sys import argv
 
 from app import app
 
 if __name__ == '__main__':
-	if len(argv) > 1:
-		port = int(argv[1])
-	else:
-		port = None
+    if len(argv) > 1:
+        port = int(argv[1])
+    else:
+        port = None
+    
     app.run(host='0.0.0.0', port=port)
